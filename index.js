361d4525cf63ec9bfaaa1867c212434c9b3b08d3
Leandro Afonso <leandro.afonso@hotmail.com>
2022-05-26 19:57:20 -0400

    multi-language implemented

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 
-const token = ""
+const token = " "
+
 const submitButton = document.getElementById("read")
 const date = document.getElementById("date")
 const textArea = document.getElementById("txt")
+const presentation = document.querySelector("#presentation")
+const enButton = document.getElementById("en")
+const ptButton = document.getElementById("pt")
+const esButton = document.getElementById("es")
 
+let version = "kjv/pv"
 
 english()
 
+ptButton.addEventListener("click", () => {
+    version = "nvi/pv"
+    presentation.innerText = "O livro de Provérbios foi escrito pelo Rei Salomão para transmitir conhecimento e sabedoria. Neste plano de leitura você poderá ler o livro inteiro em 31 dias (um capítulo por dia). Como o conteúdo deste livro pode ser aplicado independente de sua ordem, simplesmente selecione o dia de hoje e aproveite a leitura!"
+    submitButton.value = "Ler"
+})
+
+esButton.addEventListener("click", () => {
+    version = "rvr/pv"
+    presentation.innerText = "El libro de Proverbios fue escrito por el rey Salomón para ofrecer instrucción, sabiduría y entendimiento. En este plan de lectura vas a ser capaz de leer el libro completo en 31 días (un capítulo por día). como el el contenido de Proverbios es independiente de su orden, simplemente elija la fecha de hoy en el cuadro a continuación, y lo resolveremos por usted. ¡Disfrutar!"
+    submitButton.value = "Leer"
+
+})
+
+enButton.addEventListener("click", () => {document.location.reload()})
+
+
+
+
 function english(){ //added everything into a function as I'm trying to implement multiple languages
-    
     submitButton.addEventListener("click", () => {
         const day = parseInt(date.value.slice(-2), 10)
         console.log(day)
@@ -22,7 +45,7 @@ function english(){ //added everything into a function as I'm trying to implemen
                 Authorization: `Bearer ${token}`
             }
         }
-         return fetch(`https://www.abibliadigital.com.br/api/verses/kjv/pv/${n}`, enData)
+         return fetch(`https://www.abibliadigital.com.br/api/verses/${version}/${n}`, enData)
             .then((response) => response.json())
             .then((obj) => {
                 console.log(obj)
@@ -63,3 +86,4 @@ function english(){ //added everything into a function as I'm trying to implemen
     }
     
 }
+
